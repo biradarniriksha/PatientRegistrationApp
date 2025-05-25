bd0296b3effb403db937e69c5216370d396c23d1
nirikshabiradar <86408195+nirikshabiradar@users.noreply.github.com>
2025-05-25 23:09:46 +0530

    vercel implement

--- vite.config.js
+++ vite.config.js
@@ -21,18 +21,44 @@
 // })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 // import basicSsl from '@vitejs/plugin-basic-ssl'
+// export default defineConfig({
+//   plugins: [vue()],
+//   //plugins: [vue(), basicSsl()],
+//   optimizeDeps: {
+//     exclude: ['@electric-sql/pglite']
+//   },
+//   server: {
+//     headers: {
+//       'Cross-Origin-Opener-Policy': 'same-origin',
+//       'Cross-Origin-Embedder-Policy': 'require-corp',
+//       'Cross-Origin-Resource-Policy': 'cross-origin'
+//     }
+//   }
+// })
+
 export default defineConfig({
-  plugins: [vue()],
-  //plugins: [vue(), basicSsl()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_headers',   // copy from here
+          dest: '.'                 // into dist/
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['@electric-sql/pglite']
   },
+  assetsInclude: ['/.wasm', '/.json'],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Resource-Policy': 'cross-origin'
-    }
-  }
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
 })
